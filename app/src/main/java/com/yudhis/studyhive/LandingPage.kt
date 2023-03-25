0f51c654aafeb839e2b855fcb38f59a6436fab5d
yudhisthereal <63510362+yudhisthereal@users.noreply.github.com>
2023-03-25 10:54:19 +0700

    Added Activities

---
+++ app/src/main/java/com/yudhis/studyhive/LandingPage.kt
@@ -0,0 +1,11 @@
+package com.yudhis.studyhive
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LandingPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_landing_page)
+    }
+}
