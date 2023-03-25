3b895144386d90e2976b03f86a7fbea866cba562
sadrachharmasantyo <sadrach084>
2023-03-25 13:26:19 +0700

    signup to login

--- app/src/main/java/com/yudhis/studyhive/SignUp2Activity.kt
+++ app/src/main/java/com/yudhis/studyhive/SignUp2Activity.kt
@@ -3,6 +3,11 @@ package com.yudhis.studyhive
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 
@@ -11,14 +16,27 @@ class SignUp2Activity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up2)
 
+        val textViewSudahPunyaAkunLogin = "Sudah punya akun? Login"
         val buttonBuatAkun = findViewById<Button>(R.id.bt_buat_akun)
-        val textViewSudahPunyaAkunLogin = findViewById<TextView>(R.id.tv_sudah_punya_akun_login)
+//        val textViewSudahPunyaAkunLogin = findViewById<TextView>(R.id.tv_sudah_punya_akun_login)
+//
+//        textViewSudahPunyaAkunLogin.setOnClickListener {
+//            Intent(this, LoginActivity::class.java).also {
+//                startActivity(it)
+//            }
+//        }
+        val spannableString = SpannableString(textViewSudahPunyaAkunLogin)
 
-        textViewSudahPunyaAkunLogin.setOnClickListener {
-            Intent(this, LoginActivity::class.java).also {
-                startActivity(it)
+        val clickableSpan = object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                startActivity(Intent(this@SignUp2Activity, LoginActivity::class.java))
             }
         }
+        spannableString.setSpan(clickableSpan, 17, 23, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        val textView = findViewById<TextView>(R.id.tv_sudah_punya_akun)
+        textView.text = spannableString
+        textView.movementMethod = LinkMovementMethod.getInstance()
 
         buttonBuatAkun.setOnClickListener {  }
     }
--- app/src/main/java/com/yudhis/studyhive/SignupActivity.kt
+++ app/src/main/java/com/yudhis/studyhive/SignupActivity.kt
@@ -3,6 +3,11 @@ package com.yudhis.studyhive
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 
@@ -11,8 +16,9 @@ class SignupActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
 
+        val textViewSudahPunyaAkunLogin = "Sudah punya akun? Login"
         val buttonSelanjutnya = findViewById<Button>(R.id.bt_selanjutnya)
-        val textViewSudahPunyaAkunLogin = findViewById<TextView>(R.id.tv_sudah_punya_akun_login)
+//        val textViewSudahPunyaAkunLogin = findViewById<TextView>(R.id.tv_sudah_punya_akun_login)
 
         buttonSelanjutnya.setOnClickListener {
             Intent(this, SignUp2Activity::class.java).also {
@@ -20,10 +26,23 @@ class SignupActivity : AppCompatActivity() {
             }
         }
 
-        textViewSudahPunyaAkunLogin.setOnClickListener {
-            Intent(this, LoginActivity::class.java).also {
-                startActivity(it)
+        val spannableString = SpannableString(textViewSudahPunyaAkunLogin)
+
+        val clickableSpan = object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                startActivity(Intent(this@SignupActivity, LoginActivity::class.java))
             }
         }
+        spannableString.setSpan(clickableSpan, 17, 23, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        val textView = findViewById<TextView>(R.id.tv_sudah_punya_akun)
+        textView.text = spannableString
+        textView.movementMethod = LinkMovementMethod.getInstance()
+
+//        textViewSudahPunyaAkunLogin.setOnClickListener {
+//            Intent(this, LoginActivity::class.java).also {
+//                startActivity(it)
+//            }
+//        }
     }
 }
