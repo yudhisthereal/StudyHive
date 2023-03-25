086169240408054a93122093555598f9c313190b
sadrachharmasantyo <sadrach084>
2023-03-25 12:26:21 +0700

    connect activity_signup and activity_sign_up2 to Loginactivity

--- app/src/main/java/com/yudhis/studyhive/SignUp2Activity.kt
+++ app/src/main/java/com/yudhis/studyhive/SignUp2Activity.kt
@@ -1,11 +1,25 @@
 package com.yudhis.studyhive
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class SignUp2Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up2)
+
+        val buttonBuatAkun = findViewById<Button>(R.id.bt_buat_akun)
+        val textViewSudahPunyaAkunLogin = findViewById<TextView>(R.id.tv_sudah_punya_akun_login)
+
+        textViewSudahPunyaAkunLogin.setOnClickListener {
+            Intent(this, LoginActivity::class.java).also {
+                startActivity(it)
+            }
+        }
+
+        buttonBuatAkun.setOnClickListener {  }
     }
 }
--- app/src/main/java/com/yudhis/studyhive/SignupActivity.kt
+++ app/src/main/java/com/yudhis/studyhive/SignupActivity.kt
@@ -1,11 +1,29 @@
 package com.yudhis.studyhive
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        val buttonSelanjutnya = findViewById<Button>(R.id.bt_selanjutnya)
+        val textViewSudahPunyaAkunLogin = findViewById<TextView>(R.id.tv_sudah_punya_akun_login)
+
+        buttonSelanjutnya.setOnClickListener {
+            Intent(this, SignUp2Activity::class.java).also {
+                startActivity(it)
+            }
+        }
+
+        textViewSudahPunyaAkunLogin.setOnClickListener {
+            Intent(this, LoginActivity::class.java).also {
+                startActivity(it)
+            }
+        }
     }
 }
