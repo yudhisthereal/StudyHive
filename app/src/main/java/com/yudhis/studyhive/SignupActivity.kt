2c999b56cbcd200f60beb97d14409613074413c1
sadrachharmasantyo <sadrach084>
2023-03-26 14:32:05 +0700

    Changing design

--- app/src/main/java/com/yudhis/studyhive/SignupActivity.kt
+++ app/src/main/java/com/yudhis/studyhive/SignupActivity.kt
@@ -1,6 +1,7 @@
 package com.yudhis.studyhive
 
 import android.content.Intent
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
@@ -9,6 +10,7 @@ import android.text.Spanned
 import android.text.TextWatcher
 import android.text.method.LinkMovementMethod
 import android.text.style.ClickableSpan
+import android.text.style.StyleSpan
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -36,6 +38,7 @@ class SignupActivity : AppCompatActivity() {
             }
         }
         spannableString.setSpan(clickableSpan, 17, 23, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        spannableString.setSpan(StyleSpan(Typeface.BOLD),17,23,Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
 
         val textView = findViewById<TextView>(R.id.tv_sudah_punya_akun)
         textView.text = spannableString
