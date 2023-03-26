91e9c2dc0027e5996699ece57041331b5c2c08f9
yudhisthereal <63510362+yudhisthereal@users.noreply.github.com>
2023-03-26 14:16:22 +0700

    Logout

--- app/src/main/java/com/yudhis/studyhive/LandingPageActivity.kt
+++ app/src/main/java/com/yudhis/studyhive/LandingPageActivity.kt
@@ -1,11 +1,27 @@
 package com.yudhis.studyhive
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.yudhis.studyhive.LoginActivity.Companion.ACCOUNT_NAME
+import com.yudhis.studyhive.databinding.ActivityLandingPageBinding
 
 class LandingPageActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityLandingPageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_landing_page)
+        binding = ActivityLandingPageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.txtUsername.text = intent.getStringExtra(ACCOUNT_NAME)
+
+        binding.btnLogout.setOnClickListener{
+            FirebaseAuth.getInstance().signOut()
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
     }
 }
