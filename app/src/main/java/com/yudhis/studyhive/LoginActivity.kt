b77cf838203829899edca08f03c6b249b1e41b65
yudhisthereal <63510362+yudhisthereal@users.noreply.github.com>
2023-03-27 22:45:09 +0700

    New Login Starting Point

--- app/src/main/java/com/yudhis/studyhive/LoginActivity.kt
+++ app/src/main/java/com/yudhis/studyhive/LoginActivity.kt
@@ -24,27 +24,20 @@ import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.yudhis.studyhive.databinding.ActivityLoginBinding
-import java.io.InputStream
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var fBaseAuth : FirebaseAuth
+    private lateinit var auth : FirebaseAuth
     private lateinit var binding : ActivityLoginBinding
+    private lateinit var gsc : GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Google sign in
-        val gso : GoogleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken("421961381611-c33pgbcqcjuvi3r01aqjors21ogsd8rh.apps.googleusercontent.com")
-            .requestEmail()
-            .build()
-        val gsc : GoogleSignInClient = GoogleSignIn.getClient(this, gso)
-        binding.btnGoogleLogin.setOnClickListener{
-            googleSignIn(gsc)
-        }
+        // initialize firebase authorization
+        auth = Firebase.auth
 
         // make the "Daftar" part of "Belum punya akun? Daftar" clickable
         val ss = SpannableString("Belum punya akun? Daftar")
@@ -65,50 +58,34 @@ class LoginActivity : AppCompatActivity() {
 
         // regular login handling
         binding.btnLogin.setOnClickListener{
-            if (TextUtils.isEmpty(binding.fieldEmail.text)) {
+            val email = binding.fieldEmail.text
+            val password = binding.fieldPass.text
+            if (TextUtils.isEmpty(email)) {
                 binding.fieldEmail.error = "Email harus diisi"
             }
-            else if (!Patterns.EMAIL_ADDRESS.matcher(binding.fieldEmail.text).matches()) {
+            else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
                 binding.fieldEmail.error = "Email invalid"
             }
-            else {
-                //check account existence
-                if (!accountExists(binding.fieldEmail.text.toString()))
-                {
-                    binding.fieldEmail.error = "Email ini belum terdaftar"
-                    return@setOnClickListener
-                }
-            }
 
-            if (TextUtils.isEmpty(binding.fieldPass.text)) {
+            if (TextUtils.isEmpty(password)) {
                 binding.fieldPass.error = "Password harus diisi"
             }
             else {
-                //get account info
-                val accountInfo : HashMap<String, String> = getUserRecord(assets.open("users.csv"), binding.fieldEmail.text.toString())
-
-                //match password
-                if (accountInfo["UserPassword"] != binding.fieldPass.text.toString()) {
-                    binding.fieldPass.error = "Password salah"
-                }
-                else {
-                    signIn(accountInfo)
-                }
+                signIn(email.toString(), password.toString())
             }
-
         }
 
-        // initialize firebase authorization
-        fBaseAuth = Firebase.auth
-    }
-    override fun onStart() {
-        super.onStart()
-        // Check if user is signed in (non-null) and update UI accordingly.
-        val currentUser = fBaseAuth.currentUser
-        if (currentUser != null) {
-            updateUI(currentUser)
+        // Google sign in
+        val gso : GoogleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("421961381611-c33pgbcqcjuvi3r01aqjors21ogsd8rh.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
+        gsc = GoogleSignIn.getClient(this, gso)
+        binding.btnGoogleLogin.setOnClickListener{
+            signIn()
         }
     }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
@@ -126,14 +103,15 @@ class LoginActivity : AppCompatActivity() {
             }
         }
     }
+
     private fun firebaseAuthWithGoogle(idToken: String) {
         val credential = GoogleAuthProvider.getCredential(idToken, null)
-        fBaseAuth.signInWithCredential(credential)
+        auth.signInWithCredential(credential)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     // Sign in success, update UI with the signed-in user's information
                     Log.d(TAG, "signInWithCredential:success")
-                    val user = fBaseAuth.currentUser
+                    val user = auth.currentUser
                     updateUI(user)
                 } else {
                     // If sign in fails, display a message to the user.
@@ -142,80 +120,52 @@ class LoginActivity : AppCompatActivity() {
                 }
             }
     }
-    private fun updateUI(user: FirebaseUser?) {
-        if (user != null) {
-            val landingPageIntent = Intent(applicationContext, LandingPageActivity::class.java)
-            ACCOUNT_INFO["UserName"] = user.displayName.toString()
-            ACCOUNT_INFO["AccountEmail"] = user.email.toString()
-            startActivity(landingPageIntent)
-        }
-    }
-    private fun googleSignIn(gsc : GoogleSignInClient) {
+
+    // google sign in
+    private fun signIn() {
         val signInIntent = gsc.signInIntent
         startActivityForResult(signInIntent, RC_SIGN_IN)
     }
 
-    private fun signIn(accountInfo : HashMap<String, String>) {
-        val landingPageIntent = Intent(this, LandingPageActivity::class.java)
-        ACCOUNT_INFO = accountInfo
-        startActivity(landingPageIntent)
-        finish()
-        //SIGN IN
+    // password based sign in
+    private fun signIn(email : String, password : String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    val exception = task.exception
+                    Log.w(TAG, "signInWithEmail:failure", exception)
+                    Toast.makeText(baseContext, "Autentikasi Gagal",
+                        Toast.LENGTH_LONG).show()
+                    updateUI(null)
+                }
+            }
     }
 
-    private fun accountExists(email : String) : Boolean {
-        val istream = assets.open("users.csv")
-        val reader = istream.bufferedReader()
-        reader.readLine() // discard header
-        var rowStr: String = reader.readLine()
-        var found = false
-        while (rowStr.isNotBlank()) {
-            var record = rowStr.split(",").toMutableList()
-            record[1] = record[1].trim()
-            if (record[1] == email) {
-                found = true
-                break
-            }
-            try {
-                rowStr = reader.readLine()
-            } catch (e: NullPointerException) {
-                break
-            }
-        }
-        return found
+    override fun onStart() {
+        super.onStart()
+        val currentUser : FirebaseUser? = auth.currentUser
+        updateUI(currentUser)
     }
-    private fun getUserRecord(istream : InputStream, email : String) : HashMap<String, String> {
-        var record : HashMap<String, String> = hashMapOf()
-        val reader = istream.bufferedReader()
-        var header = reader.readLine().split(",").toMutableList()
-        for (i in header.indices) {
-            header[i] = header[i].trim()
+
+    private fun updateUI(user : FirebaseUser?) {
+        if (user != null) {
+            //go to landing page
+            startActivity(Intent(this@LoginActivity, LandingPageActivity::class.java))
+            finish()
         }
-        var rowStr : String = reader.readLine()
-        while(rowStr.isNotBlank() && rowStr != null) {
-            val currentRecord = rowStr.split(",").toMutableList()
-            currentRecord[1] = currentRecord[1].trim()
-            if (currentRecord[1] == email) {
-                for (i in currentRecord.indices) {
-                    currentRecord[i] = currentRecord[i].trim()
-                }
-                record = header.zip(currentRecord).toMap()  as HashMap<String, String>
-                break
-            }
-            try {
-                rowStr = reader.readLine()
-            } catch (e : NullPointerException) {
-                break
-            }
-        }
-        return record
     }
 
 
     companion object {
         private const val TAG = "GoogleActivity"
         private const val RC_SIGN_IN = 9001
-        var ACCOUNT_INFO : HashMap<String, String> = hashMapOf()
+//        var ACCOUNT_INFO : HashMap<String, String> = hashMapOf()
     }
 
 }
