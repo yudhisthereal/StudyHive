a12f2e073af8726ccd94062ad3afc8694a9a9d1b
sadrachharmasantyo <sadrach084>
2023-03-28 00:23:19 +0700

    add

---
+++ app/src/main/java/com/yudhis/studyhive/SignUpCourseActivity.kt
@@ -0,0 +1,11 @@
+package com.yudhis.studyhive
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpCourseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up_course)
+    }
+}
