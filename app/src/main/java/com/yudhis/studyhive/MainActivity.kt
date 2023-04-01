577ff192414cab104e98de9d384702f6c3081dae
yudhisthereal <63510362+yudhisthereal@users.noreply.github.com>
2023-04-01 16:42:45 +0700

    Added Content

--- app/src/main/java/com/yudhis/studyhive/MainActivity.kt
+++ app/src/main/java/com/yudhis/studyhive/MainActivity.kt
@@ -5,34 +5,41 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Person
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.platform.LocalLayoutDirection
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.yudhis.studyhive.composeables.AppBar
-import com.yudhis.studyhive.composeables.DrawerBody
-import com.yudhis.studyhive.composeables.DrawerHeader
-import com.yudhis.studyhive.composeables.MenuItem
+import com.yudhis.studyhive.dataclass.MenuItem
 import com.yudhis.studyhive.ui.theme.StudyHiveTheme
+import com.yudhis.studyhive.ui.theme.Transparent
+import com.yudhis.studyhive.composeables.MyCard
+import com.yudhis.studyhive.dataclass.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
@@ -45,18 +52,114 @@ class MainActivity : ComponentActivity() {
         val gsc = GoogleSignIn.getClient(this@MainActivity, GoogleSignInOptions.DEFAULT_SIGN_IN)
         setContent {
             StudyHiveTheme {
-                Box{
+                Box {
                     val scaffoldState = rememberScaffoldState()
                     val coroutineScope = rememberCoroutineScope()
+                    var logoutConfOpen by remember {
+                        mutableStateOf(false)
+                    }
 
                     Scaffold(
                         scaffoldState = scaffoldState,
-                        topBar = {MainAppBar(coroutineScope, scaffoldState)},
-                        drawerContent = { MainDrawerMenu(gsc) },
+                        topBar = { MainAppBar(coroutineScope, scaffoldState) },
+                        drawerContent = {
+                            MainDrawerMenu(
+                                gsc = gsc,
+                                confirmLogout = {
+                                    logoutConfOpen = true
+                                }
+                            )
+                        },
                         content = { padding ->
                             val modifier = Modifier.padding(padding)
-                            MainUI(user, modifier)
+                            val searchQuery = remember {
+                                mutableStateOf(TextFieldValue(""))
+                            }
+                            MainUI(user, modifier, searchQuery)
                         }
+                    )
+                    if (logoutConfOpen) {
+                        LogoutConfirmation(
+                            onDismiss = { logoutConfOpen = false },
+                            gsc = gsc
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun MainUI(user : FirebaseUser?, modifier : Modifier, searchState: MutableState<TextFieldValue>) {
+        Box(
+            modifier = modifier
+                .background(MaterialTheme.colors.primary)
+                .fillMaxWidth()
+                .fillMaxHeight()
+        ) {
+            Column()
+            {
+                Greeting(user)
+                Spacer(modifier = Modifier.height(10.dp))
+                Box {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .offset(y = 48.dp)
+                            .shadow(
+                                elevation = 3.dp,
+                                shape = RoundedCornerShape(topStart = 48.dp, topEnd = 48.dp),
+                                clip = true
+                            )
+                            .background(MaterialTheme.colors.background)
+                    ) {
+
+                    }
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier.padding(24.dp)
+                    ) {
+                        SearchBar(
+                            searchState = searchState,
+                            onQueryChanged = {newQuery ->
+                                searchState.value = newQuery
+                            }
+                        )
+                        Spacer(
+                            modifier = Modifier
+                                .height(32.dp)
+                                .padding(16.dp)
+                        )
+                        DiscoverCoursesAndEvents()
+                        //CourseSuggestions()
+                    }
+                }
+            }
+        }
+    }
+
+    @Composable
+    private fun CourseSuggestions() {
+        TODO("Not yet implemented")
+    }
+
+    @Composable
+    private fun DiscoverCoursesAndEvents() {
+        Column{
+            Text(
+                text = "Discover Things!",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colors.onBackground
+            )
+            LazyRow {
+                items(count = 73) {id ->
+                    MyCard(
+                        size = Large,
+                        cornerRadius = 8.dp,
+                        text = "Cool Course $id",
+                        imagePainter = painterResource(id = R.drawable.img_demo_course),
+                        description = "Course Card $id"
                     )
                 }
             }
@@ -64,33 +167,49 @@ class MainActivity : ComponentActivity() {
     }
 
     @Composable
-    private fun MainUI(user : FirebaseUser?, modifier : Modifier) {
-        Box(
-            modifier = modifier
-                .background(MaterialTheme.colors.primary)
-                .fillMaxWidth()
-                .fillMaxHeight()
-                .padding(24.dp)
-        ) {
-            Column()
-            {
-                Spacer(modifier = Modifier
-                    .height(16.dp))
-                Greeting(user)
-            }
-        }
+    private fun SearchBar(searchState:MutableState<TextFieldValue>, onQueryChanged: (newQuery:TextFieldValue) -> Unit) {
+        TextField(
+            value = searchState.value,
+            onValueChange = {
+                onQueryChanged(it)
+            },
+            trailingIcon = {
+                Icon(
+                    imageVector = ImageVector.vectorResource(com.google.android.material.R.drawable.ic_search_black_24),
+                    contentDescription = null
+                )
+            },
+            placeholder = {Text(text = "Cari Kursus atau Event")},
+            singleLine = true,
+            shape = RoundedCornerShape(32.dp),
+            modifier = Modifier
+                .fillMaxWidth(0.8f)
+                .shadow(elevation = 3.dp, shape = RoundedCornerShape(32.dp), clip = true)
+                .background(color = MaterialTheme.colors.background)
+        )
     }
 
     @Composable
     fun Greeting(user : FirebaseUser?) {
-        Text(text = "Welcome,", fontSize = 24.sp)
-        Text(text = user?.displayName.toString() + "!", fontSize = 32.sp)
+
+        Text(
+            text = "Welcome,",
+            fontSize = 24.sp,
+            color = MaterialTheme.colors.onPrimary,
+            modifier = Modifier.padding(start = 32.dp)
+            )
+        Text(
+            text = user?.displayName.toString() + "!",
+            fontSize = 32.sp,
+            color = MaterialTheme.colors.onPrimary,
+            fontWeight = FontWeight.Black,
+            modifier = Modifier.padding(start = 32.dp))
     }
 
     @Composable
-    private fun MainAppBar(coroutineScope: CoroutineScope, scaffoldState : ScaffoldState) {
+    fun MainAppBar(coroutineScope: CoroutineScope, scaffoldState : ScaffoldState) {
         AppBar(
-            onNavigationIconClicked = {
+            onMenuClicked = {
                 coroutineScope.launch {
                     scaffoldState.drawerState.open()
                 }
@@ -99,8 +218,78 @@ class MainActivity : ComponentActivity() {
     }
 
     @Composable
-    private fun MainDrawerMenu(gsc : GoogleSignInClient) {
-        DrawerHeader()
+    fun LogoutConfirmation(onDismiss : () -> Unit, gsc : GoogleSignInClient)
+    {
+        Dialog(
+            onDismissRequest = { onDismiss() },
+            content = {
+                Surface(
+                    elevation = 4.dp,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(8.dp)),
+                ) {
+                    Column(
+                        verticalArrangement = Arrangement.SpaceAround,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier
+                            .padding(16.dp)
+                    ) {
+                        Text(
+                            text = "Keluar dari StudyHive?",
+                            fontWeight = FontWeight.Normal,
+                            fontSize = 16.sp
+                        )
+                        Spacer(
+                            modifier = Modifier
+                                .height(16.dp)
+                        )
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceEvenly,
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Button(
+                                colors = ButtonDefaults.buttonColors(MaterialTheme.colors.primary),
+                                onClick = {
+                                    gsc.signOut().addOnCompleteListener { task ->
+                                        if (task.isSuccessful) {
+                                            FirebaseAuth.getInstance().signOut()
+                                            finish()
+                                            startActivity(
+                                                Intent(
+                                                    this@MainActivity,
+                                                    LoginActivity::class.java
+                                                )
+                                            )
+                                        }
+                                    }
+                                }
+                            )
+                            {
+                                Text(text = "Ya", color = MaterialTheme.colors.onPrimary)
+                            }
+                            Button(
+                                colors = ButtonDefaults.buttonColors(Transparent),
+                                elevation = ButtonDefaults.elevation(0.dp),
+                                onClick = {
+                                    onDismiss()
+                                }
+                            )
+                            {
+                                Text(text = "Tidak", color = MaterialTheme.colors.onBackground)
+                            }
+                        }
+                    }
+                }
+            }
+        )
+    }
+
+    @Composable
+    fun MainDrawerMenu(gsc : GoogleSignInClient, confirmLogout : () -> Unit) {
+
         DrawerBody(
             items = listOf(
 //                                    MenuItem(
@@ -128,60 +317,122 @@ class MainActivity : ComponentActivity() {
                     icon = ImageVector.vectorResource(id = R.drawable.ic_logout)
                 )
             ),
-            modifier = Modifier
-                .padding(8.dp),
+            modifier = Modifier,
             itemTextStyle = TextStyle.Default,
             onItemClick = { item ->
                 when (item.id) {
                     "account_info" -> {
-                        // TODO : Go to account info
+                        TODO("not yet implemented")
                     }
                     "participants" -> {
-                        // TODO : Go to participants detail
+                        TODO("not yet implemented")
                     }
                     "logout" -> {
-                        gsc.signOut().addOnCompleteListener { task ->
-                            if (task.isSuccessful) {
-                                FirebaseAuth.getInstance().signOut()
-                                finish()
-                                startActivity(
-                                    Intent(
-                                        this@MainActivity,
-                                        LoginActivity::class.java
-                                    )
-                                )
-                            }
-                        }
+                         confirmLogout()
                     }
                 }
             }
         )
     }
+
+    @Composable
+    fun DrawerBody(
+        items : List<MenuItem>,
+        modifier : Modifier,
+        itemTextStyle : TextStyle,
+        onItemClick : (MenuItem) -> Unit
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colors.background)
+                .padding(24.dp)
+        ) {
+            Text(
+                text = "Menu",
+                fontWeight = FontWeight.Black,
+                fontSize = 32.sp
+            )
+
+            Spacer(modifier = Modifier.width(16.dp))
+
+            LazyColumn(
+                modifier = modifier
+                    .fillMaxSize(),
+            ) {
+                items(items = items) { item ->
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clip(RoundedCornerShape(8.dp))
+                            .clickable {
+                                onItemClick(item)
+                            }
+                            .padding(16.dp)
+                    ) {
+                        Icon(
+                            imageVector = item.icon,
+                            contentDescription = item.description,
+                            tint = contentColorFor(backgroundColor = MaterialTheme.colors.background)
+                        )
+                        Spacer(modifier = Modifier.width(16.dp))
+                        Text(
+                            text = item.text,
+                            style = itemTextStyle,
+                            modifier = Modifier
+                                .weight(1f),
+                            fontSize = 18.sp,
+                            color = contentColorFor(backgroundColor = MaterialTheme.colors.background)
+                        )
+                    }
+                }
+            }
+        }
+    }
     @Preview(showBackground = true)
     @Composable
     fun AppPreview()
     {
-        Box(
+        Surface(
+            elevation = 4.dp,
             modifier = Modifier
-                .padding(0.dp, 100.dp, 0.dp, 0.dp)
-                .background(Color(0,0,0,0))
-        )
-        {
-            Box(
-                modifier = Modifier
-                    .background(MaterialTheme.colors.primary)
-                    .fillMaxWidth()
-                    .fillMaxHeight()
-                    .padding(32.dp)
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(16.dp))
+                .padding(32.dp),
+            color = MaterialTheme.colors.onPrimary
+        ) {
+            Column(
+                verticalArrangement = Arrangement.SpaceAround,
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Column()
-                {
-                    Spacer(
-                        modifier = Modifier
-                            .height(16.dp)
+
+                Text(
+                    text = "Keluar dari StudyHive?"
+                )
+                Spacer(
+                    modifier = Modifier
+                        .height(16.dp)
+                )
+                Row(
+                    horizontalArrangement = Arrangement.SpaceAround,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Button(
+                        colors = ButtonDefaults.buttonColors(MaterialTheme.colors.primary),
+                        onClick = {},
                     )
-                    Text(text = "Welcome,", fontSize = 24.sp)
-                    Text(text = "YUDHIS!", fontSize = 32.sp)
+                    {
+                        Text(text = "Ya", color = MaterialTheme.colors.onPrimary)
+                    }
+                    Button(
+                        colors = ButtonDefaults.buttonColors(MaterialTheme.colors.onPrimary),
+                        onClick = {
+
+                        }
+                    )
+                    {
+                        Text(text = "Tidak", color = MaterialTheme.colors.primary)
+                    }
                 }
             }
         }
--- app/src/main/java/com/yudhis/studyhive/composeables/MenuDrawer.kt
+++ app/src/main/java/com/yudhis/studyhive/composeables/MenuDrawer.kt
@@ -5,32 +5,19 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.Composable
 import androidx.compose.material.Text
-import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.material.contentColorFor
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-
-
-@Composable
-fun DrawerHeader() {
-    Text(
-        text = "Menu",
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(32.dp),
-        fontSize = 24.sp,
-        fontWeight = FontWeight(200)
-    )
-}
+import com.yudhis.studyhive.dataclass.MenuItem
 
 @Composable
 fun DrawerBody(
@@ -39,27 +26,49 @@ fun DrawerBody(
     itemTextStyle : TextStyle,
     onItemClick : (MenuItem) -> Unit
 ) {
-    LazyColumn(modifier = modifier) {
-        items(items = items) { item ->
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .clickable {
-                        onItemClick(item)
-                    }
-                    .padding(8.dp)
-            ) {
-                Icon(
-                    imageVector = item.icon,
-                    contentDescription = item.description
-                )
-                Spacer(modifier = Modifier.width(16.dp))
-                Text(
-                    text = item.text,
-                    style = itemTextStyle,
-                    modifier = Modifier.weight(1f),
-                    fontSize = 18.sp
-                )
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colors.background)
+            .padding(24.dp)
+    ) {
+        Text(
+            text = "Menu",
+            fontWeight = FontWeight.Black,
+            fontSize = 32.sp
+        )
+
+        Spacer(modifier = Modifier.width(16.dp))
+
+        LazyColumn(
+            modifier = modifier
+                .fillMaxSize(),
+        ) {
+            items(items = items) { item ->
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(8.dp))
+                        .clickable {
+                            onItemClick(item)
+                        }
+                        .padding(16.dp)
+                ) {
+                    Icon(
+                        imageVector = item.icon,
+                        contentDescription = item.description,
+                        tint = contentColorFor(backgroundColor = MaterialTheme.colors.background)
+                    )
+                    Spacer(modifier = Modifier.width(16.dp))
+                    Text(
+                        text = item.text,
+                        style = itemTextStyle,
+                        modifier = Modifier
+                            .weight(1f),
+                        fontSize = 18.sp,
+                        color = contentColorFor(backgroundColor = MaterialTheme.colors.background)
+                    )
+                }
             }
         }
     }
